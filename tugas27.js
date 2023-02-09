3c8403740aa5d666cf4bda8343f1f655cc8535e5
dnriski <nugrohoriski159@gmail.com>
2023-02-09 18:48:14 +0700

    first commit

---
+++ tugas27.js
@@ -0,0 +1,11 @@
+var a = "No";
+var b = "rumah saya";
+var c = 30;
+var d = "A";
+
+console.log(a, b, String(c), d);
+
+var e = "Jalan pangeran Tirtayasa No";
+var f = 70;
+
+console.log(e, String(f));
